21c47e72ab13003650aa2af4bd5c7ca3b717d003
Deep Shah <deepshah89@outlook.com>
2018-05-30 10:25:25 +0530

    Create SettingsData

---
+++ SettingsData.py
@@ -0,0 +1,14 @@
+import wx
+
+Default_PreferredScanner = "USB Cam"
+Default_Font = "Helvetica"
+Default_FontSize = 24
+Default_FontColor = wx.BLACK
+Default_IsSaveImages = "No"
+
+PreferredScanner = "USB Cam"
+Font = "Helvetica"
+FontSize = 24
+FontColor = wx.BLACK
+IsSaveImages = "No"
+
